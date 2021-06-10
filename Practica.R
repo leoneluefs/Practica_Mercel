63499133aac3eb1c9cb220d6cf42dd8efd9824ac
leoneluefs <73231797+leoneluefs@users.noreply.github.com>
2021-06-09 23:18:07 -0300

    Create Practica.R

---
+++ Practica.R
@@ -0,0 +1,14 @@
+install.packages('crul')
+
+# ggplot2
+# geobr
+# raster
+# fields
+# ggspatial
+# sf
+
+install.packages("ggplot2")
+install.packages("geobr")
+install.packages("fields")
+install.packages("sf")
+install.packages("ggspatial")
